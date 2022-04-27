7917819a89d787392f23113ceba738067e0e9604
threadexio <pzarganitis@gmail.com>
2022-04-27 18:39:13 +0300

    gruvbox-like rice

--- scripts/scripts/init.sh
+++ scripts/scripts/init.sh
@@ -3,21 +3,19 @@
 killall -wq picom
 killall -wq polybar
 
-xrandr --output DP-1-4 --off
-xrandr --output HDMI-1-0 --off
+if [ "$1" == "restart" ]; then
 
-i3-msg restart
+	xrandr --output DP-1-4 --off
+	xrandr --output HDMI-1-0 --off
 
-sleep 1
+	i3-msg restart
 
-xrandr --output DP-1-4 --auto --right-of eDP1
-xrandr --output HDMI-1-0 --auto --right-of DP-1-4 #--rotate right # doesnt work with i3 for some reason
+	sleep 1
 
-sleep 2
+	xrandr --output DP-1-4 --auto --right-of eDP1
+	xrandr --output HDMI-1-0 --auto --right-of DP-1-4 #--rotate right # doesnt work with i3 for some reason
 
-if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-
-swaybg -m fill -i ~/.local/share/wallpapers/current
+	sleep 1
 
 else
 
@@ -25,19 +23,23 @@ else
 	xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1
 	xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Disable While Typing Enabled' 0
 
-	picom -b --experimental-backends &!
-
-	feh --no-fehbg --bg-fill ~/.local/share/wallpapers/current
-
-	screens=($(xrandr | sed -nr 's|^(.*) connected.*$|\1|p'))
-	for screen in "${screens[@]}"; do
-		polybar "$screen" >/dev/null 2>&1 &!
-	done
-
 fi
 
-dunst &!
+picom -b --experimental-backends &!
 
-sleep 1
+feh --no-fehbg --bg-fill ~/.local/share/wallpapers/current
 
-~/.config/scripts/xdg-autostart.sh
+screens=($(xrandr | sed -nr 's|^(.*) connected.*$|\1|p'))
+for screen in "${screens[@]}"; do
+	polybar "$screen" >/dev/null 2>&1 &!
+done
+
+if [ "$1" != "restart" ]; then
+
+	dunst &!
+
+	sleep 1
+
+	~/.config/scripts/xdg-autostart.sh
+
+fi
